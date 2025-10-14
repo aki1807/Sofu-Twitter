b28d74a4e6aa3d1b35ada0dfa5f92cb2e39f3e9d
aki1807 <akkihiro20060707@gmail.com>
2025-10-14 20:01:06 +0900

    create.htmlの仮完成

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from . import views
  
 urlpatterns = [
     path("", views.home, name = "home"),
-    path("", views.create, name = "create"),
+    path("create/", views.create, name = "create"),
 ]
