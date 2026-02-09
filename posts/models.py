a5260af19224c67ace6d6fd683fe99075b3c3c72
unknown <taiseipkd@outlook.jp>
2026-02-10 00:09:19 +0900

    AI実装

--- posts/models.py
+++ posts/models.py
@@ -5,8 +5,8 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    content = models.CharField(max_length=280)  # 投稿内容（ツイート本文）
-    created_at = models.DateTimeField(default=timezone.now)  # 投稿日時
+    content = models.CharField(max_length=280)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.content[:20]  # 管理画面で最初の20文字だけ表示
+        return self.content[:20] 
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .ai_check import check_politeness
 
-# Create your views here.
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')  # 新しい順に並べる
-    return render(request, "posts/home.html",{'posts': posts})
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, "posts/home.html", {'posts': posts})
+
+# 投稿画面
+def create(request):
+    error_message = None
+
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        if content:
+            # AI判定を実行
+            is_polite, reason = check_politeness(content)
+
+            if is_polite:
+                # 保存してホーム
+                Post.objects.create(content=content)
+                return redirect('home')
+            else:
+                #エラーメッセージを出して、そのまま投稿画面に留まる
+                error_message = reason
+
+    return render(request, "posts/create.html", {'error_message': error_message})
